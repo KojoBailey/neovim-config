ee1058c379397a4c7a3c99fb57447beea53c19a2
Kojo Bailey <kojomolojo@gmail.com>
2026-02-05 15:00:52 +0000

    Add a bunch of plugins

--- lua/kojo/init.lua
+++ lua/kojo/init.lua
@@ -10,7 +10,20 @@ if vim.fn.has("win32") == 1 then
 	vim.opt.shellxquote = ""
 end
 
+if vim.fn.has("persistent_undo") == 1 then
+    local target_path = vim.fn.expand('~/.undodir')
+
+    if vim.fn.isdirectory(target_path) == 0 then
+        vim.fn.mkdir(target_path, "p", 0700)
+    end
+
+    vim.opt.undodir = target_path
+    vim.opt.undofile = true
+end
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.termguicolors = true
+
+vim.g.undotree_DiffCommand = "C:/Program Files/Git/usr/bin/diff.exe"
--- lua/kojo/lazy/kanagawa.lua
+++ lua/kojo/lazy/kanagawa.lua
@@ -1,18 +1,18 @@
 return {
-  "rebelot/kanagawa.nvim",
-  lazy = false,    -- Load on startup
-  priority = 1000, -- Load before everything else
-  config = function()
-    require('kanagawa').setup({
-      compile = false,
-      undercurl = true,
-      commentStyle = { italic = true },
-      keywordStyle = { italic = true },
-      transparent = true,
-      theme = "wave", -- Try "dragon" if you want it darker
-    })
+	"rebelot/kanagawa.nvim",
+	lazy = false,    -- Load on startup
+	priority = 1000, -- Load before everything else
+	config = function()
+		require('kanagawa').setup({
+			compile = false,
+			undercurl = true,
+			commentStyle = { italic = true },
+			keywordStyle = { italic = true },
+			transparent = true,
+			theme = "wave", -- Try "dragon" if you want it darker
+		})
 
-    -- Activate the theme
-    vim.cmd("colorscheme kanagawa")
-  end,
+		-- Activate the theme
+		vim.cmd("colorscheme kanagawa")
+	end,
 }
