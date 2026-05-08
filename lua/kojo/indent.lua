1cf6f4a2032ff43e9a525d7c8057c4539593f5be
Kojo Bailey <kojomolojo@gmail.com>
2026-05-08 16:52:38 +0100

    Add support for yuck (eww)

--- lua/kojo/indent.lua
+++ lua/kojo/indent.lua
@@ -18,7 +18,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "html" },
+	pattern = { "html", "css", "qml" },
 	callback = function()
 		vim.opt_local.shiftwidth = 2
 		vim.opt_local.tabstop = 2
